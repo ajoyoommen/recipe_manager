1f7b8cfa46201c2445c0fcdb0709bba91b15d4f9
Ajoy Oommen <ajoyoommen@gmail.com>
2021-07-04 18:02:06 +0530

    Add ingredient search feature

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('home', views.home, name='home'),
     path('recipe/<int:recipe_id>', views.get_recipe, name='recipe-detail'),
-    path('ingredients', views.get_ingredients, name='ingredients'),
+    path('ingredients', views.IngredientList.as_view(), name='ingredients'),
     path('ingredient/<int:ingredient_id>', views.get_ingredient, name='ingredient-detail'),
     path('ingredient/<int:pk>/update', views.UpdateIngredient.as_view(), name='ingredient-edit'),
     path('ingredient/add', views.CreateIngredient.as_view(), name='ingredient-add')
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView
 
 from recipes import models
@@ -18,11 +19,20 @@ def get_recipe(request, recipe_id):
     })
 
 
-def get_ingredients(request):
-    ingredients = models.Ingredient.objects.order_by('name')
-    return render(request, 'ingredients/list.html', {
-        'ingredients': ingredients
-    })
+class IngredientList(ListView):
+    template_name = 'ingredients/list.html'
+    model = models.Ingredient
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+        object_list = self.model.objects.all()
+        if q:
+            _objs_name = object_list.filter(name__icontains=q)
+            _objs_artn = object_list.filter(article_number__icontains=q)
+            object_list =  _objs_name | _objs_artn
+        print(f'Name: `{q}`, queryset: {object_list}')
+        return object_list
+
 
 def get_ingredient(request, ingredient_id):
     ingredient = get_object_or_404(models.Ingredient, pk=ingredient_id)
