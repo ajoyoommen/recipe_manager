f371e276e2e04ec92bd1b64e3208b4bf88b9a703
Ajoy Oommen <ajoyoommen@gmail.com>
2021-08-21 19:42:57 +0530

    Add setup.py and tox config

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="Simple recipe manger",
+    version="0.3",
+    url="",
+    author="Ajoy Oommen",
+    author_email="ajoyoommen@gmail.com",
+    description="A simple recipe manager built using Django",
+    packages=["recipe_manager"],
+)
